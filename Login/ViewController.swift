e8b1bc058fd386979d7669e90b3f83ff09426e7a
Justin Thompson <justindthomps@gmail.com>
2020-01-22 11:12:24 -0600

    init commit

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var showPassSwitch: UISwitch!
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var forgotUser: UIButton!
+    @IBOutlet var forgotPass: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showPassSwitched(_ sender: Any) {
+        if(showPassSwitch.isOn){
+            passwordTextField.isSecureTextEntry = false
+        }
+        if(showPassSwitch.isOn == false){
+            passwordTextField.isSecureTextEntry = true
+        }
+    }
+    
+    
+    @IBAction func forgotUserAction(_ sender: Any) {
+        performSegue(withIdentifier: "LogIn", sender: forgotUser)
+    }
+    @IBAction func forgotPassAction(_ sender: Any) {
+        performSegue(withIdentifier: "LogIn", sender: forgotPass)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let sender = sender as? UIButton else {return}
+        
+        
+        if sender == forgotPass {
+            segue.destination.navigationItem.title = "Forgot Password"
+        }
+        else if sender == forgotUser {
+            segue.destination.navigationItem.title = "Forgot Username"
+        }
+        else {
+            segue.destination.navigationItem.title = usernameTextField.text
+        }
+    }
+    
 }
 
